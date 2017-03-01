51d6fda56cf5b2b1a49c5bd5a06b5db1fcea7d54
lazrad <lazarradulovic93@gmail.com>
2017-03-01 20:27:43 +0100

    tut1.1

--- LTP001.py
+++ LTP001.py
@@ -1,1 +1,13 @@
-print("is it working?")
+# Programming basically involves listing all the things that must
+# happen in order to solve a problem
+# Determine step by step what needs to happen
+# And then you need to convert those step into a programming language
+
+
+# Ask  the user to input their name and assign it to a variable named name
+
+name = input("What is your name? ")
+
+# Print out hello followed by the name they entered
+
+print("Hello ", name)
